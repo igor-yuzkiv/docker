514e746e45f30f35715cd4f2b4257e78d3646fc8
bood92 <igor97w@gmail.com>
2020-03-05 16:46:45 +0200

    nginx_mysql-5.7_php-7.4_laravel-6.x

--- nginx_mysql-5.7_php-7.4_laravel-6.x/install.sh
+++ nginx_mysql-5.7_php-7.4_laravel-6.x/install.sh
@@ -4,6 +4,6 @@ cd ./Nginx/www
 mkdir -p html
 
 docker exec php-fpm-7.4_laravel composer create-project --prefer-dist laravel/laravel ./
+docker exec php-fpm-7.4_laravel composer update
 
-
-
+sudo chgrp -R www-data Nginx/www/ && sudo chmod -R a+rwx Nginx/www/
