ed746c4034cb47aa91721db2ff058c234d21606a
bood92 <igor97w@gmail.com>
2020-03-05 15:55:20 +0200

    nginx_mysql-5.7_php-7.4_laravel-6.x

--- nginx_mysql-5.7_php-7.4_laravel-6.x/install.sh
+++ nginx_mysql-5.7_php-7.4_laravel-6.x/install.sh
@@ -3,6 +3,7 @@ docker-compose up -d
 cd ./Nginx/www
 mkdir html
 
-docker exec c php-fpm-7.4_laravel composer create-project --prefer-dist laravel/laravel ./
+docker exec php-fpm-7.4_laravel composer create-project --prefer-dist laravel/laravel ./
+
 
 
